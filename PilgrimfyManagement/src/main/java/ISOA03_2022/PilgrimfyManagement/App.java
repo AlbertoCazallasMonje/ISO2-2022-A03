7ec3e884ee331c9ea71830a20b40515860915a5a
andresitocc99 <andres.castellanos1@alu.uclm.es>
2022-12-02 05:01:22 +0100

    First Commit from eclipse

---
+++ PilgrimfyManagement/src/main/java/ISOA03_2022/PilgrimfyManagement/App.java
@@ -0,0 +1,13 @@
+package ISOA03_2022.PilgrimfyManagement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
