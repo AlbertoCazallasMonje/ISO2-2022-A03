934e5b41a3e2abe64a6756ed6cfa600896b8b646
Marvin Schmidt <Marvin.Schmidt@alu.uclm.es>
2022-12-09 11:09:30 +0100

    add blank PilgrimfyServerApp

---
+++ PilgrimfyServerApp/src/main/java/ISOA03_2022/PilgrimfyServerApp/App.java
@@ -0,0 +1,12 @@
+package ISOA03_2022.PilgrimfyServerApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println( "This is the Pilgrimfy Server Application!" );
+    }
+}
