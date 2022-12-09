0cb64d3d34743b1344ebc405753cb258657814af
Marvin Schmidt <Marvin.Schmidt@alu.uclm.es>
2022-12-09 11:09:21 +0100

    add blank PilgrimfyPilgrimsApp

--- PilgrimfyManagement/src/main/java/ISOA03_2022/PilgrimfyManagement/App.java
+++ PilgrimfyManagement/src/main/java/ISOA03_2022/PilgrimfyManagement/App.java
@@ -7,6 +7,6 @@ package ISOA03_2022.PilgrimfyManagement;
 public class App 
 {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "This is the Pilgrimfy Management / Business Owner Application!" );
     }
 }
---
+++ PilgrimfyPilgrimsApp/src/main/java/ISOA03_2022/PilgrimfyPilgrimsApp/App.java
@@ -0,0 +1,12 @@
+package ISOA03_2022.PilgrimfyPilgrimsApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println( "This is the Pilgrimfy Pilgrims Application!" );
+    }
+}
