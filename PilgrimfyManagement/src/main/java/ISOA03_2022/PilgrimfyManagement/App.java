231e88298b588a1cad7c5b3c437cafcb7cb80e04
Marvin Schmidt <Marvin.Schmidt@alu.uclm.es>
2022-12-09 11:01:03 +0100

    fixed Maven build errors

--- PilgrimfyManagement/src/main/java/ISOA03_2022/PilgrimfyManagement/App.java
+++ PilgrimfyManagement/src/main/java/ISOA03_2022/PilgrimfyManagement/App.java
@@ -6,8 +6,7 @@ package ISOA03_2022.PilgrimfyManagement;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
