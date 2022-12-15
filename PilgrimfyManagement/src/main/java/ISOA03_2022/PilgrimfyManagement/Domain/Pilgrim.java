a619cf64c880af3398dd32339630eef023024cf1
Alejandro Durán <115149516+AlejandroAluUclm@users.noreply.github.com>
2022-12-15 21:06:46 +0100

    Iteration 5

---
+++ PilgrimfyManagement/src/main/java/ISOA03_2022/PilgrimfyManagement/Domain/Pilgrim.java
@@ -0,0 +1,14 @@
+package Static_View.Domain;
+
+public class Pilgrim {
+	private String _username;
+	private String _nIF;
+	private String _email;
+	private Array<Pilgrim_Travel> _pilgrim_Travel;
+	private Array<Comment> _comment;
+	public Comment _unnamed_Comment_;
+
+	public Pilgrim(String aUsername, String aNIF, String aEmail) {
+		throw new UnsupportedOperationException();
+	}
+}
