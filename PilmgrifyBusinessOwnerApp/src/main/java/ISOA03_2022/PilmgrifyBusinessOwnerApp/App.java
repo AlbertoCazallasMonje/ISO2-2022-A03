2dc1545cdc1c24392b72680f4920936b7986d9ca
AlbertoCazallasMonje <alberto.cazallas@alu.uclm.es>
2022-12-15 18:31:10 +0100

    PilmgrifyBusinessOwnerApp created
    Delete

---
+++ PilmgrifyBusinessOwnerApp/src/main/java/ISOA03_2022/PilmgrifyBusinessOwnerApp/App.java
@@ -0,0 +1,13 @@
+package ISOA03_2022.PilmgrifyBusinessOwnerApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
