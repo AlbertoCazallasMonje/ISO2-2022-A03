074a4838aea491a8bb6aabbe3a28efe98886beec
Alejandro Durán <115149516+AlejandroAluUclm@users.noreply.github.com>
2022-12-15 21:07:14 +0100

    Add files via upload

---
+++ PilgrimfyPilgrimsApp/src/main/java/ISOA03_2022/PilgrimfyPilgrimsApp/Domain/Business.java
@@ -0,0 +1,12 @@
+package Static_View.Domain;
+
+public class Business {
+	private String _name;
+	private String _id;
+	private String _contact;
+	public Comment _unnamed_Comment_;
+
+	public void Businnes(String aName, String aId, String aContact) {
+		throw new UnsupportedOperationException();
+	}
+}
