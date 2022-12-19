7c3fd6d0a0043e5227a6ddb81d7889bfbe267dbd
Alejandro Durán <115149516+AlejandroAluUclm@users.noreply.github.com>
2022-12-19 11:55:36 +0100

    Iteration 7

---
+++ PilgrimfyPilgrimsApp/src/main/java/ISOA03_2022/PilgrimfyPilgrimsApp/Domain/BusinessOwner.java
@@ -0,0 +1,16 @@
+package Static_View.Domain;
+
+public class BusinessOwner {
+	private String _id;
+	private String _contact;
+	private String _fullName;
+	public Offer _unnamed_Offer_;
+
+	public BusinessOwner(String aId, String aContact, String aFullName) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void createOffer() {
+		throw new UnsupportedOperationException();
+	}
+}
