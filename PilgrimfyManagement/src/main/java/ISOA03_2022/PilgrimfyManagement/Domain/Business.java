0ae2d2a8c607f07da94b79439923f775f87561cf
Alejandro Durán <115149516+AlejandroAluUclm@users.noreply.github.com>
2022-12-19 11:54:52 +0100

    Iteration 7

---
+++ PilgrimfyManagement/src/main/java/ISOA03_2022/PilgrimfyManagement/Domain/Business.java
@@ -0,0 +1,12 @@
+package Static_View.Domain;
+
+public class Business {
+	private String _name;
+	private String _id;
+	private String _contact;
+	public Offer _unnamed_Offer_;
+
+	public Business(String aName, String aId, String aContact) {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ PilgrimfyManagement/src/main/java/ISOA03_2022/PilgrimfyManagement/Domain/Offer.java
@@ -0,0 +1,24 @@
+package Static_View.Domain;
+
+public class Offer {
+	private String _type;
+	private float _price;
+	private String _description;
+	public BusinessOwner _unnamed_BusinessOwner_;
+	public Pilgrim _unnamed_Pilgrim_;
+	public Business _unnamed_Business_;
+	public Pilgrim_Travel _unnamed_Pilgrim_Travel_;
+	public Offer_DAO _unnamed_Offer_DAO_;
+
+	public Offer(String aType, float aPrice, String aDescription) {
+		throw new UnsupportedOperationException();
+	}
+
+	public Offer makeOffer() {
+		throw new UnsupportedOperationException();
+	}
+
+	public Offer showOffer() {
+		throw new UnsupportedOperationException();
+	}
+}
